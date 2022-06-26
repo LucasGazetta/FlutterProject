4ae9cdad243dc6306c487fd96eece9a8ff1b8570
LucasGazetta <gazetinhagames@gmail.com>
2022-06-26 19:28:36 -0300

    correcoes

--- imc_flutter/lib/constants/app_constants.dart
+++ imc_flutter/lib/constants/app_constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:hexcolor/hexcolor.dart';
 
 final Color mainHexColor = HexColor("#1C1C1C");
